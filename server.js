5a0ba61c620f4ab1c1759f283e9343cf3669c924
Akshay <rathodak315@gmail.com>
2021-06-21 10:23:19 +0530

    'initial'

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const path = require('path');
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+// set template engine
+app.use(expressLayout);
+
+app.set('views',path.join(__dirname,'resources/views'));
+
+app.set('view engine','ejs');
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server is started successfully on ports ${PORT}`);
+})
